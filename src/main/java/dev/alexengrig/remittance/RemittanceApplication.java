8b1124dff68e1b69477536f968e6983729110a3d
Grig Alex <alexengrigdev@gmail.com>
2020-12-25 19:58:28 +0300

    Initial commit

---
+++ src/main/java/dev/alexengrig/remittance/RemittanceApplication.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.remittance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemittanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemittanceApplication.class, args);
+    }
+
+}
