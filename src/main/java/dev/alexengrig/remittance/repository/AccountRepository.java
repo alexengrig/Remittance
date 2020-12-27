b290e5e5f470b419cc1949b265428f5478309d17
Grig Alex <alexengrigdev@gmail.com>
2020-12-27 03:58:30 +0300

    Create AccountRepository

---
+++ src/main/java/dev/alexengrig/remittance/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package dev.alexengrig.remittance.repository;
+
+import dev.alexengrig.remittance.domain.Account;
+
+import java.util.Optional;
+
+public interface AccountRepository {
+
+    Optional<Account> findById(long accountId);
+}
