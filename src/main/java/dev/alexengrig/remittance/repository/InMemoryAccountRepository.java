9510099cf05ccf4ae34e7a8ed9ee40c87faec2ac
Grig Alex <alexengrigdev@gmail.com>
2020-12-31 02:28:24 +0300

    Add pullData and pushData to InMemoryAccountRepository

--- src/main/java/dev/alexengrig/remittance/repository/InMemoryAccountRepository.java
+++ src/main/java/dev/alexengrig/remittance/repository/InMemoryAccountRepository.java
@@ -1,4 +1,13 @@
 package dev.alexengrig.remittance.repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public interface InMemoryAccountRepository extends AccountRepository {
+
+    @PostConstruct
+    void pullData();
+
+    @PreDestroy
+    void pushData();
 }
