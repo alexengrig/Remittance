bbdb0a8507eeb9e59778246c257a8ffff02f4e74
Grig Alex <alexengrigdev@gmail.com>
2021-01-06 01:03:11 +0300

    Draft

--- src/test/java/dev/alexengrig/remittance/service/SimpleMoneyTransferServiceConcurrentTest.java
+++ src/test/java/dev/alexengrig/remittance/service/SimpleMoneyTransferServiceConcurrentTest.java
@@ -4,32 +4,54 @@ import dev.alexengrig.remittance.domain.Account;
 import dev.alexengrig.remittance.domain.AtomicAccount;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SimpleMoneyTransferServiceConcurrentTest extends SimpleMoneyTransferServiceBaseTest {
 
-    @Test
-    void should_transfer_money_in_circle_for_atomicAccounts() throws InterruptedException {
-        Account account1 = new AtomicAccount(1L, 100);
-        Account account2 = new AtomicAccount(2L, 200);
-        Account account3 = new AtomicAccount(3L, 300);
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        executorService.execute(() -> moneyTransferService.doTransfer(account1, account2, 100));
-        executorService.execute(() -> moneyTransferService.doTransfer(account2, account3, 200));
-        executorService.execute(() -> moneyTransferService.doTransfer(account3, account1, 300));
+    void testTransferMoneyInCircle(List<Account> accounts, List<Long> amounts) throws InterruptedException {
+        assertEquals(accounts.size(), amounts.size(), "The size of accounts doesn't equal to the size of amounts");
+        Balancer balancer = Balancer.of(accounts);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        int last = accounts.size() - 1;
+        for (int i = 0; i < last; i++) {
+            int index = i;
+            executorService.execute(() -> {
+                long amount = amounts.get(index);
+                balancer.move(index, index + 1, amount);
+                moneyTransferService.doTransfer(accounts.get(index), accounts.get(index + 1), amount);
+            });
+        }
+        executorService.execute(() -> {
+            long amount = amounts.get(last);
+            balancer.move(last, 0, amount);
+            moneyTransferService.doTransfer(accounts.get(last), accounts.get(0), amount);
+        });
         executorService.shutdown();
-        if (!executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+        if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
             executorService.shutdownNow();
             fail();
         }
-        assertEquals(300, account1.getBalance(), "The account1 balance is incorrect");
-        assertEquals(100, account2.getBalance(), "The account2 balance is incorrect");
-        assertEquals(200, account3.getBalance(), "The account3 balance is incorrect");
+        for (int i = 0; i < accounts.size(); i++) {
+            Account account = accounts.get(i);
+            assertEquals(balancer.get(i), account.getBalance(), "The account balance is incorrect: " + account);
+        }
+    }
+
+    @Test
+    void should_transfer_money_in_circle_for_atomicAccounts() throws InterruptedException {
+        testTransferMoneyInCircle(
+                Arrays.asList(new AtomicAccount(1L, 100), new AtomicAccount(2L, 200), new AtomicAccount(3L, 300)),
+                Arrays.asList(100L, 200L, 300L));
     }
 
     @Test
@@ -51,5 +73,29 @@ public class SimpleMoneyTransferServiceConcurrentTest extends SimpleMoneyTransfe
         assertEquals(0, account2.getBalance(), "The account2 balance is incorrect");
         assertEquals(0, account3.getBalance(), "The account3 balance is incorrect");
         assertEquals(600, account4.getBalance(), "The account4 balance is incorrect");
+    }
+
+    static class Balancer {
+        private final List<AtomicLong> balances;
+
+        static Balancer of(List<Account> accounts) {
+            return new Balancer(accounts.stream()
+                    .map(Account::getBalance)
+                    .map(AtomicLong::new)
+                    .collect(Collectors.toList()));
+        }
+
+        private Balancer(List<AtomicLong> balances) {
+            this.balances = new CopyOnWriteArrayList<>(balances);
+        }
+
+        void move(int from, int to, long amount) {
+            balances.get(from).addAndGet(-amount);
+            balances.get(to).addAndGet(amount);
+        }
+
+        long get(int index) {
+            return balances.get(index).get();
+        }
     }
 }
